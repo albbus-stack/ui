4e569bf2f0de511ba9ce972938fb4c5405cfc6f1
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-05-25 21:28:10 +0200

    docs: modified sink to a card grid

--- apps/www/app/sink/page.tsx
+++ apps/www/app/sink/page.tsx
@@ -51,151 +51,227 @@ export default function KitchenSinkPage() {
   return (
     <div className="container">
       <div className="grid gap-4 py-10">
-        <div className="grid grid-cols-3 items-start gap-4">
-          <div className="grid gap-4">
-            <ComponentWrapper>
-              <CardDemo className="w-full" />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <SliderDemo className="w-full" />
-            </ComponentWrapper>
-            <ComponentWrapper
-              className="spa flex-col items-start space-x-0
-				space-y-2"
-            >
-              <p className="text-foreground-muted text-sm">Documentation</p>
-              <p className="text-sm font-medium leading-none">
-                You can customize the theme using{" "}
-                <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold text-foreground">
-                  CSS variables
-                </code>
-                .{" "}
-                <Link
-                  href="#"
-                  className="font-medium text-primary underline underline-offset-4"
-                >
-                  Click here
-                </Link>{" "}
-                to learn more.
-              </p>
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <CheckboxDemo />
-              <HoverCardDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="[&>div]:w-full">
-              <TabsDemo />
-            </ComponentWrapper>
-          </div>
-          <div className="grid gap-4">
-            <ComponentWrapper>
-              <MenubarDemo />
-              <AvatarDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="flex-col items-start space-x-0 space-y-2">
-              <div className="flex space-x-2">
-                <ButtonDemo />
-                <ButtonSecondary />
-                <ButtonDestructive />
-              </div>
-              <div className="flex space-x-2">
-                <ButtonOutline />
-                <ButtonLink />
-                <ButtonGhost />
-              </div>
-              <div className="flex space-x-2">
-                <ButtonWithIcon />
-                <ButtonLoading />
-              </div>
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <DatePickerDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <AccordionDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="[&_ul>li:last-child]:hidden">
-              <NavigationMenuDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="justify-between">
-              <SwitchDemo />
-              <SelectDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <SeparatorDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <AspectRatioDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <PopoverDemo />
-              <ToastDemo />
-            </ComponentWrapper>
-          </div>
-          <div className="grid gap-4">
-            <ComponentWrapper>
-              <TooltipDemo />
-              <SheetDemo />
-              <ProgressDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <CommandDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="[&>span]:h-[80px] [&>span]:w-[200px]">
-              <RadioGroupDemo />
-              <ContextMenuDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <div className="flex space-x-2">
-                <DropdownMenuDemo />
-                <AlertDialogDemo />
-                <DialogDemo />
-              </div>
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <div className="flex space-x-2">
-                <BadgeDemo />
-                <BadgeSecondary />
-                <BadgeDestructive />
-                <BadgeOutline />
-              </div>
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <SkeletonDemo />
-            </ComponentWrapper>
-            <ComponentWrapper className="[&>div]:w-full">
-              <CollapsibleDemo />
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <div className="flex space-x-2">
-                <ToggleDemo />
-                <ToggleOutline />
-                <ToggleDisabled />
-                <ToggleWithText />
-              </div>
-            </ComponentWrapper>
-            <ComponentWrapper>
-              <ScrollAreaDemo />
-            </ComponentWrapper>
-          </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 2xl:grid-cols-3">
+          <ComponentCard title="Card" description="A simple card component">
+            <CardDemo className="w-[80%]" />
+          </ComponentCard>
+          <ComponentCard
+            title="Slider"
+            description="A slider component"
+            className="mix-blend-multiply dark:mix-blend-screen"
+          >
+            <SliderDemo className="w-[80%]" />
+          </ComponentCard>
+          <ComponentCard title="Checkbox" description="A checkbox component">
+            <CheckboxDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Hover Card"
+            description="An hover card component"
+          >
+            <HoverCardDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Tabs"
+            description="A tabs component"
+            className="[&>div]:w-[80%]"
+          >
+            <TabsDemo />
+          </ComponentCard>
+          <ComponentCard title="Menubar" description="A menubar component">
+            <MenubarDemo />
+          </ComponentCard>
+          <ComponentCard title="Avatar" description="An avatar component">
+            <AvatarDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Button"
+            description="Some button components"
+            className="flex-col gap-4 py-10 mix-blend-multiply dark:mix-blend-screen"
+          >
+            <div className="flex space-x-2">
+              <ButtonDemo />
+              <ButtonSecondary />
+              <ButtonDestructive />
+            </div>
+            <div className="flex space-x-2">
+              <ButtonOutline />
+              <ButtonLink />
+              <ButtonGhost />
+            </div>
+            <div className="flex space-x-2">
+              <ButtonWithIcon />
+              <ButtonLoading />
+            </div>
+          </ComponentCard>
+
+          <ComponentCard
+            title="Date Picker"
+            description="A date picker component"
+          >
+            <DatePickerDemo />
+          </ComponentCard>
+
+          <ComponentCard
+            title="Accordion"
+            description="An accordion component"
+            className="mx-auto w-[80%] py-10"
+          >
+            <AccordionDemo />
+          </ComponentCard>
+
+          <ComponentCard
+            title="Navigation Menu"
+            description="A navigation menu component"
+            className="[&_ul>li:last-child]:hidden"
+          >
+            <NavigationMenuDemo />
+          </ComponentCard>
+
+          <ComponentCard title="Switch" description="A switch component">
+            <SwitchDemo />
+          </ComponentCard>
+          <ComponentCard title="Select" description="A select component">
+            <SelectDemo />
+          </ComponentCard>
+          <ComponentCard title="Separator" description="A separator component">
+            <SeparatorDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Aspect Ratio"
+            description="An aspect ratio component"
+            className="w-[80%] mx-auto"
+          >
+            <AspectRatioDemo />
+          </ComponentCard>
+          <ComponentCard title="Popover" description="A popover component">
+            <PopoverDemo />
+          </ComponentCard>
+          <ComponentCard title="Toast" description="A toast component">
+            <ToastDemo />
+          </ComponentCard>
+          <ComponentCard title="Tooltip" description="A tooltip component">
+            <TooltipDemo />
+          </ComponentCard>
+          <ComponentCard title="Sheet" description="A sheet component">
+            <SheetDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Progress"
+            description="A progress component"
+            className="mix-blend-multiply dark:mix-blend-screen"
+          >
+            <ProgressDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Command"
+            description="A command component"
+            className="w-[80%] mx-auto"
+          >
+            <CommandDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Radio"
+            description="A radio component"
+            className="[&>span]:h-[80px] [&>span]:w-[200px]"
+          >
+            <RadioGroupDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Context Menu"
+            description="A context menu component"
+            className="[&>span]:h-[80px] [&>span]:w-[200px]"
+          >
+            <ContextMenuDemo />
+          </ComponentCard>
+          <ComponentCard title="Dropdown" description="A dropdown component">
+            <DropdownMenuDemo />
+          </ComponentCard>
+          <ComponentCard
+            title="Alert Dialog"
+            description="An alert dialog component"
+          >
+            <AlertDialogDemo />
+          </ComponentCard>
+          <ComponentCard title="Dialog" description="A dialog component">
+            <DialogDemo />
+          </ComponentCard>
+          <ComponentCard title="Badge" description="A badge component">
+            <BadgeDemo />
+            <BadgeSecondary />
+            <BadgeDestructive />
+            <BadgeOutline />
+          </ComponentCard>
+
+          <ComponentCard
+            title="Skeleton"
+            description="A skeleton component"
+            className="w-[80%] mx-auto mix-blend-multiply dark:mix-blend-screen"
+          >
+            <SkeletonDemo />
+          </ComponentCard>
+
+          <ComponentCard
+            title="Collapsible"
+            description="A collapsible component"
+            className="[&>div]:w-[80%]"
+          >
+            <CollapsibleDemo />
+          </ComponentCard>
+
+          <ComponentCard title="Toggle" description="A toggle component">
+            <div className="flex space-x-2">
+              <ToggleDemo />
+              <ToggleOutline />
+              <ToggleDisabled />
+              <ToggleWithText />
+            </div>
+          </ComponentCard>
+
+          <ComponentCard
+            title="Scroll Area"
+            description="A scroll area component"
+          >
+            <ScrollAreaDemo />
+          </ComponentCard>
         </div>
       </div>
     </div>
   )
 }
 
-function ComponentWrapper({
-  className,
+interface ComponentCardProps {
+  title: string
+  description: string
+  children: React.ReactNode
+  href?: string
+  className?: string
+}
+
+function ComponentCard({
+  title,
+  description,
   children,
-}: React.HTMLAttributes<HTMLDivElement>) {
+  className,
+  href,
+}: ComponentCardProps) {
   return (
-    <div
-      className={cn(
-        "flex items-center justify-between space-x-4 rounded-md border p-4",
-        className
-      )}
-    >
-      {children}
+    <div className="flex flex-col transition-transform border rounded-lg bg-secondary text-secondary-foreground">
+      <section
+        className={cn(
+          "min-h-[150px] flex py-10 mt-auto justify-center items-center",
+          className
+        )}
+      >
+        {children}
+      </section>
+      <Link
+        href={href ?? "#"}
+        className="w-full p-5 mt-auto transition-opacity bg-white rounded-lg rounded-t-none dark:bg-accent hover:opacity-[.85]"
+      >
+        <h3 className="mb-3 text-xl font-bold leading-none">{title}</h3>
+        <p className="text-sm text-foreground opacity-60">{description}</p>
+      </Link>
     </div>
   )
 }
